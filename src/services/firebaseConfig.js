2fe2bd8f19d6a89a200d35ee223dc5a9671af8cd
Eric Wajnrajch <ericwaje03@gmail.com>
2022-10-27 11:28:08 -0300

    creando variables de entorno

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDloq2ILiQYLIIQacrLJvDSCs_xKQiQY7U',
-    authDomain: 'react-comision-34775.firebaseapp.com',
-    projectId: 'react-comision-34775',
-    storageBucket: 'react-comision-34775.appspot.com',
-    messagingSenderId: '435051624763',
-    appId: '1:435051624763:web:96f472e5624da1780f1f1e',
+    apiKey: REACT_APP_API_KEY,
+    authDomain: REACT_APP_AUTH_DOMAIN,
+    projectId: REACT_APP_PROJECT_ID,
+    storageBucket: REACT_APP_STORAGE,
+    messagingSenderId: REACT_APP_MESSAGING,
+    appId: REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
