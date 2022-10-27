4ff1ed2fe3497e64e040e2572bd36efbd44e0826
Eric Wajnrajch <ericwaje03@gmail.com>
2022-10-27 11:31:18 -0300

    agregando procces.env

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: REACT_APP_API_KEY,
-    authDomain: REACT_APP_AUTH_DOMAIN,
-    projectId: REACT_APP_PROJECT_ID,
-    storageBucket: REACT_APP_STORAGE,
-    messagingSenderId: REACT_APP_MESSAGING,
-    appId: REACT_APP_APP_ID,
+    apiKey: procces.env.REACT_APP_API_KEY,
+    authDomain: procces.env.REACT_APP_AUTH_DOMAIN,
+    projectId: procces.env.REACT_APP_PROJECT_ID,
+    storageBucket: procces.env.REACT_APP_STORAGE,
+    messagingSenderId: procces.env.REACT_APP_MESSAGING,
+    appId: procces.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
