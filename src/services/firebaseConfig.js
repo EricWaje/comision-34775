298a6edfe7b6cf2e9dd50bd0febf7b60982c2418
Eric Wajnrajch <ericwaje03@gmail.com>
2022-10-27 11:32:04 -0300

    cambiando la palabra

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: procces.env.REACT_APP_API_KEY,
-    authDomain: procces.env.REACT_APP_AUTH_DOMAIN,
-    projectId: procces.env.REACT_APP_PROJECT_ID,
-    storageBucket: procces.env.REACT_APP_STORAGE,
-    messagingSenderId: procces.env.REACT_APP_MESSAGING,
-    appId: procces.env.REACT_APP_APP_ID,
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE,
+    messagingSenderId: process.env.REACT_APP_MESSAGING,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
